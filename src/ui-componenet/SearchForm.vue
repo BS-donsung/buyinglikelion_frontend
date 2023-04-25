4a665be566744ff38f8d052f6c15f68aab501310
tmfrldu <121007155+tmfrldu@users.noreply.github.com>
2023-04-25 16:50:32 +0900

    searchform

---
+++ src/ui-componenet/SearchForm.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container-center-horizontal">
+    <div class="searchform screen">
+      <div class="overlap-group">
+        <input class="inputbox" type="text" placeholder="검색어를 입력해 주세요." />
+        <img class="searchic" src="@asset/icon/search.svg" alt="searchic" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'SearchForm',
+}
+</script>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.searchform {
+  align-items: flex-start;
+  background-color: white;
+  border: 1px none;
+  display: flex;
+  height: 120px;
+  padding: 0 41px;
+  width: 640px;
+}
+
+.overlap-group {
+  align-items: center;
+  border-bottom-style: solid;
+  border-bottom-width: 0.5px;
+  border-color: black;
+  display: flex;
+  gap: 237px;
+  height: 50px;
+  margin-top: 35px;
+  min-width: 557px;
+  padding: 4px 0;
+}
+
+.inputbox {
+  align-self: flex-end;
+  font-family: var(--font-family-noto_sans);
+  font-size: 28px;
+  line-height: normal;
+  min-height: 38px;
+  min-width: 280px;
+  border: none;
+}
+
+.inputbox:focus {
+  outline: none;
+  box-shadow: none;
+  border: none;
+}
+
+.searchic {
+  height: 29px;
+  margin-top: 6px;
+  width: 29px
+}
+</style>
